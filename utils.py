37ec3baa1c93879f2c1cad2264a2fbeffe5d3bd3
Georgi-Tyufekchiev <gtyufekchiev@gmail.com>
2022-11-25 11:07:02 +0100

    Add Fiat-Shamir Heuristic
    The hash is SHAKE256

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import math
 import numpy as np
 from Crypto.Random import get_random_bytes as rng
 from Crypto.Util.number import bytes_to_long
+from Crypto.Hash import SHAKE256
+from Crypto.Util.number import long_to_bytes
 
 
 def vanderMatrix(N, t):
@@ -12,7 +14,7 @@ def vanderMatrix(N, t):
     :param t: Threshold
     :return: vander matrix
     """
-    x = np.array([n for n in range(1, N+1)])
+    x = np.array([n for n in range(1, N + 1)])
     n = N - t
     return np.vander(x, n, increasing=True)
 
@@ -33,3 +35,17 @@ def challenge(parties):
     entropyBytes = math.floor((math.log(x, 2) / parties / 8))
     challenges = [bytes_to_long(rng(entropyBytes)) for _ in range(parties)]
     return challenges
+
+
+def fsh(PK, C):
+    # y=g1^alhpa
+    y = PK[0][1]
+    data = ""
+    for i in range(3):
+        data += str(y[i])
+
+    for i in range(3):
+        data += str(C[i])
+
+    hashObj = SHAKE256.new(data=data.encode('utf-8'))
+    return hashObj
